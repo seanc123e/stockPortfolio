e0716b49c2626ba1b70b8c839a0872125cacbcec
seanc123e <seanc123e@yahoo.com>
2023-09-08 14:02:44 -0500

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('tbody tr').each(function (i, ele) {
+      console.log($(ele).children().first().text());
+    });
+  });
+  
+  // Feetbook
+  // Yamazon
+  // Snoozechat
